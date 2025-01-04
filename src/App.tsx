36bdfb9fad01ee61a546d72af260c893247e2e50
Leonardo Garcia <leonardogarcia1914@hotmail.com>
2025-01-03 21:33:32 -0300

    Criando NavBar e ShowCase

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <header className="bg-black opacity-60 flex justify-items-end px-2 py-4">
+          <nav>
+            <ul>
+              <li>INICIO</li>
+              <li>SOBRE</li>
+              <li>HABILIDADES</li>
+              <li>PROJETOS</li>
+              <li>CONTATO</li>
+            </ul>
+          </nav>
+        </header>
+        <section>
+            <div>
+              <h1>OI, EU SOU O LEONARDO!</h1>
+              <span>A PROFESSIONAL WEB DEVELOPER</span>
+            </div>
+        </section>
+        <section>
+            <h2>SOBRE MIM</h2>
+            <div>
+              <img src="" alt="Imagem Autor" />  
+              <p>Meu nome é Leonardo, tenho 21 anos e sou estudante de Análise e Desenvolvimento de Sistemas. Atualmente estou estudando desenvolvimento web e mobile. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit, veritatis! Dolores quae ab nostrum eveniet laborum nisi architecto earum, mollitia nulla unde iusto dolorum quibusdam exercitationem velit. Quae, nobis magnam.</p>
+            </div>         
+        </section>
+        <section>
+            <h2>HABILIDADES</h2>
+            <div>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>TypeScript</li>
+                <li>React</li>
+                <li>NextJs</li>
+                <li>TailwindCss</li>
+                <li>PostgreSQL</li>
+                <li>Python</li>
+              </ul>
+            </div>
+        </section>
+        <section>
+            <h2>PROJETOS</h2>
+            <div>
+              <div>
+                <img src="" alt="Imagem Projeto" />
+                <h3></h3>
+                <p>Projeto 1</p>
+              </div>
+              <div>
+                <img src="" alt="Imagem Projeto" />
+                <h3></h3>
+                <p>Projeto 2</p>
+              </div>
+              <div>
+                <img src="" alt="Imagem Projeto" />
+                <h3></h3>
+                <p>Projeto 3</p>
+              </div>
+            </div>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App
