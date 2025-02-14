3ff379ba1a098015ce16668375e6ef440be09147
Leonardo Garcia <leonardogarcia1914@hotmail.com>
2025-02-14 00:22:17 -0300

    adjust image path

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,10 @@
-
+import FotoPerfil from '../assets/Foto_Perfil.jpeg'
 
 const Home = () => {
   return (
-    <section id='home' className="bg-imagem_professional bg-cover bg-center h-screen flex justify-center items-center">
-        <div className="flex flex-col items-center text-white">
+    <section id='home' className="h-screen flex justify-center items-center">
+      <img className="absolute z-0 h-full w-full bg-cover bg-center" src={FotoPerfil} alt="Foto Leonardo" />
+        <div className="absolute z-10 flex flex-col items-center text-white">
             <h1 className="font-bold text-center text-3xl md:text-5xl">Olá, eu sou o Leonardo!</h1>
             <span className="text-xs md:text-lg tracking-[0.1rem]  border-b border-white py-1">A PROFESSIONAL WEB DEVELOPER</span>
         </div>
