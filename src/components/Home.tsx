96ff8b1d540890ed56a53e60f8e54f5cfae497b3
Leonardo Garcia <leonardogarcia1914@hotmail.com>
2025-02-19 12:02:54 -0300

    Adjusting details

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <section id="home" className="relative h-screen flex justify-center items-center">
       <img src={Foto_Perfil} className="w-full h-full object-cover" alt="Foto de Perfil" />
-      <div className="absolute inset-0 bg-black/40"></div>
+      <div className="absolute inset-0 bg-black/50"></div>
       <div className={`absolute ${navBarOpen ? 'z-0' : 'z-10'} flex flex-col items-center text-white`}>
         <h1 className="font-bold text-center text-3xl md:text-5xl">Olá, eu sou o Leonardo!</h1>
         <span className="text-xs md:text-lg tracking-[0.1rem] border-b border-white py-1">
