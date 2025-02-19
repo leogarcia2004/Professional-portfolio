ea9415efd0cf86c86fb0ebdfd856c32a64447c19
Leonardo Garcia <leonardogarcia1914@hotmail.com>
2025-02-19 11:51:59 -0300

    Adjusting details

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,21 @@
 
 import { useWidthContext } from "../context/WidthContext";
+import Foto_Perfil from '../assets/foto_perfil.jpeg';
 
 const Home = () => {
 
   const { navBarOpen } = useWidthContext();
 
   return (
-    <section id='home' className="bg-imagem_professional bg-cover bg-center h-screen flex justify-center items-center">
-        <div className={`absolute ${navBarOpen ? 'z-0' : 'z-10'} flex flex-col items-center text-white`}>
-            <h1 className="font-bold text-center text-3xl md:text-5xl">Olá, eu sou o Leonardo!</h1>
-            <span className="text-xs md:text-lg tracking-[0.1rem]  border-b border-white py-1">A PROFESSIONAL WEB DEVELOPER</span>
-        </div>
+    <section id="home" className="relative h-screen flex justify-center items-center">
+      <img src={Foto_Perfil} className="w-full h-full object-cover" alt="Foto de Perfil" />
+      <div className="absolute inset-0 bg-black/40"></div>
+      <div className={`absolute ${navBarOpen ? 'z-0' : 'z-10'} flex flex-col items-center text-white`}>
+        <h1 className="font-bold text-center text-3xl md:text-5xl">Olá, eu sou o Leonardo!</h1>
+        <span className="text-xs md:text-lg tracking-[0.1rem] border-b border-white py-1">
+          A PROFESSIONAL WEB DEVELOPER
+        </span>
+      </div>
     </section>
   )
 }
